6f8e5d7228e280753b72e1002c2e8d209dcc0842
Nickjohn18 <81334326+Nickjohn18@users.noreply.github.com>
2021-06-06 15:13:59 -0400

    display boxing for content

---
+++ index.js
@@ -0,0 +1,17 @@
+var apiURL = "https://api.rawg.io/api/platforms?";
+// key=
+var rawApi = "7daf1ca7cbaf4dff8d122f5a6bcb4160";
+
+
+
+
+    // fetch(apiURL)
+    // .then(function(gameResponse){ 
+    //     console.log(gameResponse)
+    //     return gameResponse.json();
+    // })
+    // .then(function(data){ 
+    //     console.log(data);
+    // })
+
+
